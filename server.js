e16a341ec0236d226aaa6fe0f7b72657734db1e0
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-04-12 17:11:59 +0200

    Added some todos and get queries

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+        id:1,
+        description: 'Take kids from school',
+        completed: false
+    },{
+        id:2,
+        description: 'Go to doctor',
+        completed: false
+    },
+    {
+        id:4,
+        description: 'Kiss me',
+        completed: true
+    }];
 
 app.get('/', function(req, resp) {
    resp.send('To do API root');
+});
+
+app.get('/todos', function (req, resp) {
+   resp.json(todos);
+});
+
+app.get('/todos/:id', function (req, resp) {
+    var id = parseInt(req.params.id),
+        matchedTodo;
+
+    todos.forEach(function(todo){
+        if(todo.id === id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if(matchedTodo) {
+        resp.json(matchedTodo);
+    } else {
+        resp.status(404).send('No entry with such id!');
+    }
 });
 
 app.listen(PORT, function() {
